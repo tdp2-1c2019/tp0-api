703984f948733bcec691b50f98d6aba3620ac91b
gocasey <gonzak6@gmail.com>
2019-03-17 10:32:00 -0300

    Agrego imagen de portada y link de descarga

--- i.js
+++ i.js
@@ -5,7 +5,18 @@ const port = process.env.PORT || 3000
 const books = google.books('v1')
 
 const search = async q => {
-  return books.volumes.list({ q })
+  let params = {
+    q : q,
+    projection: "full"
+  };
+  return books.volumes.list(params)
+}
+
+function getDownloadLink(bookAccessInfo){
+  if (bookAccessInfo.pdf.isAvailable && typeof bookAccessInfo.pdf.downloadLink != 'undefined'){
+    return bookAccessInfo.pdf.downloadLink;
+  }
+  return "";
 }
 
 var app = express()
@@ -21,9 +32,11 @@ app.get('/api/books', async function (req, res) {
       result.data.items.forEach(b => {
         r.items.push({
           name: b.volumeInfo.title,
+          cover: b.volumeInfo.imageLinks.smallThumbnail,
           authors: b.volumeInfo.authors,
           description: b.volumeInfo.description,
-          categories: b.volumeInfo.categories
+          categories: b.volumeInfo.categories,
+          downloadLink: getDownloadLink(b.accessInfo)
         })
       })
       res.status(200).json(r)
